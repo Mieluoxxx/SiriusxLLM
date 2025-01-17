d9199b2b8fe104579c8f3077fca1852dc0d46d2a
Morgan Woods <weiyiding0@gmail.com>
2025-01-17 18:43:34 +0800

    项目改由Ninja构建；完善Debug功能

--- test/test_example/example.cpp
+++ test/test_example/example.cpp
@@ -2,7 +2,7 @@
  * @Author: Morgan Woods weiyiding0@gmail.com
  * @Date: 2025-01-04 19:09:35
  * @LastEditors: Morgan Woods weiyiding0@gmail.com
- * @LastEditTime: 2025-01-06 13:31:08
+ * @LastEditTime: 2025-01-17 18:27:18
  * @FilePath: /SiriusX-infer/test/test_example/example.cpp
  * @Description: 
  */
@@ -12,6 +12,9 @@
 int add(const int a, const int b) { return a + b; }
 
 TEST(Example, AdditionTest) {
-    EXPECT_EQ(add(1, 2), 3);
+    int a = 1;
+    int b = 2;
+    int c = add(a, b);
+    EXPECT_EQ(c, 3);
     LOG(INFO) << "AdditionTest.BasicTest passed.";
 }
--- test/test_tensor/test_buffer.cpp
+++ test/test_tensor/test_buffer.cpp
@@ -1,17 +1,18 @@
-/***
+/*** 
  * @Author: Morgan Woods weiyiding0@gmail.com
  * @Date: 2025-01-04 17:40:31
  * @LastEditors: Morgan Woods weiyiding0@gmail.com
- * @LastEditTime: 2025-01-07 16:31:30
+ * @LastEditTime: 2025-01-15 21:01:23
  * @FilePath: /SiriusX-infer/test/test_tensor/test_buffer.cpp
- * @Description: 测试Buffer类
+ * @Description: 
  */
 #include <gtest/gtest.h>
 
 #include "base/buffer.h"
 
 // use_external = false, Buffer需要对内存进行管理
-TEST(test_buffer, allocate) {
+// 测试 Buffer 在管理内存时的分配功能
+TEST(test_buffer, allocate) { 
     using namespace base;
     auto alloc = base::CPUDeviceAllocatorFactory::get_instance();
     Buffer buffer(32, alloc);
@@ -20,6 +21,7 @@ TEST(test_buffer, allocate) {
 
 // use_external = true, Buffer不对内存进行管理
 // 需要手动释放
+// 测试 Buffer 在不管理外部内存时的行为
 TEST(test_buffer, use_external) {
     using namespace base;
     auto alloc = base::CPUDeviceAllocatorFactory::get_instance();
@@ -31,6 +33,7 @@ TEST(test_buffer, use_external) {
 
 // 查看Buffer的资源释放时机
 // allocate_time, 因为曾推出局部作用于后，没有被其他引用，会在HERE1和HERE2之间释放
+// 验证 Buffer 在离开作用域时自动释放内存
 TEST(test_buffer, allocate_time) {
     using namespace base;
     auto alloc = base::CPUDeviceAllocatorFactory::get_instance();
@@ -38,18 +41,19 @@ TEST(test_buffer, allocate_time) {
         Buffer buffer(32, alloc);
         ASSERT_NE(buffer.ptr(), nullptr);
         LOG(INFO) << "HERE1";
-    }
+    }   // buffer释放
     LOG(INFO) << "HERE2";
 }
 
 // allocate_time2有外部引用，所以要等整个函数执行结束后才释放
+// 验证 Buffer 在通过 std::shared_ptr 管理时的内存释放时机
 TEST(test_buffer, allocate_time2) {
     using namespace base;
     auto alloc = base::CPUDeviceAllocatorFactory::get_instance();
     std::shared_ptr<Buffer> buffer;
     {
         buffer = std::make_shared<Buffer>(32, alloc);
-    }
+    }   // buffer释放
     LOG(INFO) << "HERE";
     ASSERT_NE(buffer->ptr(), nullptr);
 }
